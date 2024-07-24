6fad4ce8725ce7208e7cd04fe3032d3bdd4145e9
dragos <andreica.dragos@yahoo.com>
2024-07-24 12:39:35 +0300

    Commit_2

--- download_and_extract.py
+++ download_and_extract.py
@@ -3,16 +3,13 @@ import os
 import requests
 
 def download_and_extract(url, extract_to='arhiva'):
-    # Descărcarea arhivei
     response = requests.get(url)
     with open("arhiva.zip", "wb") as f:
         f.write(response.content)
 
-    # Extrage fișierele din arhivă
     with zipfile.ZipFile("arhiva.zip", "r") as zip_ref:
         zip_ref.extractall(extract_to)
 
-    # Verifică fișierele extrase
     for root, dirs, files in os.walk(extract_to):
         for file in files:
             print(file)
